a66f6deb98354846f25d413e756cd75d1f969d38
SHIVAYADAV99 <107849626+SHIVAYADAV99@users.noreply.github.com>
2025-04-02 18:28:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/',
   plugins: [react()],
 })
-export default defineConfig({
-  base: "./", // Ensures correct relative paths
-  plugins: [react()],
-});
